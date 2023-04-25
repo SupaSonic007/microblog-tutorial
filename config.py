7d716612a695c07b287b13f1f5460754050e1aed
Joshua Kisnorbo <84158931+joush007@users.noreply.github.com>
2023-04-25 17:04:22 +1000

    ajax

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    
     ADMINS = ['admin@example.com']
 
     LANGUAGES = ['en', 'es']
